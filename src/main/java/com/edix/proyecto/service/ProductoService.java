138f32293c1a0f8a048e24436fcd9af802e0280f
[Ixchel Lopez] <[ixchellopezalegre@gmail.com]>
2023-02-18 11:05:17 +0100

    ProductoService actualizado

--- src/main/java/com/edix/proyecto/service/ProductoService.java
+++ src/main/java/com/edix/proyecto/service/ProductoService.java
@@ -1,5 +1,16 @@
 package com.edix.proyecto.service;
 
+import java.util.List;
+
+import com.edix.proyecto.beans.Producto;
+
 public interface ProductoService {
 
+	List<Producto>buscarTodos();
+	Producto buscarProducto(int idProducto);
+	Producto buscarPorNombre(String nombre);
+	boolean registrarProducto(Producto producto);
+	boolean borrarProducto(int idProducto);
+	boolean modificarProducto(Producto producto);
+	
 }
--- src/main/java/com/edix/proyecto/service/ProductoServiceImpl.java
+++ src/main/java/com/edix/proyecto/service/ProductoServiceImpl.java
@@ -1,5 +1,47 @@
 package com.edix.proyecto.service;
 
-public class ProductoServiceImpl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.edix.proyecto.beans.Producto;
+import com.edix.proyecto.repository.ProductoRepository;
+
+public class ProductoServiceImpl implements ProductoService{
+
+	@Autowired
+	ProductoRepository pRepo;
+
+	@Override
+	public List<Producto> buscarTodos() {
+		return pRepo.findAll();
+	}
+
+	@Override
+	public Producto buscarProducto(int idProducto) {
+		return pRepo.findById(idProducto).orElse(null);
+	}
+
+	@Override
+	public Producto buscarPorNombre(String nombre) {
+		return null;
+	}
+
+	@Override
+	public boolean registrarProducto(Producto producto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean borrarProducto(int idProducto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean modificarProducto(Producto producto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
