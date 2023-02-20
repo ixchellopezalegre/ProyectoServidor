b9b36529615b6ba8f8c7b57ccf8b23bc2605839e
[Ixchel Lopez] <[ixchellopezalegre@gmail.com]>
2023-02-20 14:01:25 +0100

    Reset of "PruebaController" and name changed. Created TarjetaRepository, TarjetaService and TarjetaServiceImpl. Created Test class and package

---
+++ src/main/java/com/edix/proyecto/rest/PruebaController.java
@@ -0,0 +1,10 @@
+package com.edix.proyecto.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/prueba")
+public class PruebaController {
+
+}
